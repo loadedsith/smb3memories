c1ed4d020e832833c431022bb0e915bea1793fb3
Graham Heath <heathg@google.com>
2017-09-06 11:27:38 -0500

    push dist to gh-pages

--- src/app/actions/index.js
+++
@@ -1,8 +0,0 @@
-import * as types from '../constants/ActionTypes';
-
-export function chooseCard(row, column) {
-  return {type: types.CHOOSE_CARD, row, column};
-}
-export function checkMatches() {
-  return {type: types.CHECK_MATCHES};
-}
--- src/app/constants/ActionTypes.js
+++
@@ -1,2 +0,0 @@
-export const CHECK_MATCHES = 'CHECK_MATCHES';
-export const CHOOSE_CARD = 'CHOOSE_CARD';
